e9ffc46249c0b83bd4aad254f31a016f211df552
Rohit Mondal <rohitmondal.22@nshm.edu.in>
2026-01-02 12:22:44 +0530

    added g-analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+// Google Analytics
+const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+if (GA_MEASUREMENT_ID) {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', GA_MEASUREMENT_ID);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
